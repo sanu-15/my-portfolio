52186742c5cce4f337d7334eadc22355e942afff
Saniya Paswan <saniyapaswan5@gmail.com>
2025-12-04 22:08:14 +0530

    Fix Vite build for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+  plugins: [react()],
+  root: ".", // Project root
+  build: {
+    outDir: "dist", // Build output folder
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"),
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+});
