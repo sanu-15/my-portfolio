ed578340ea7e1c79813f10473f556c6cd4a5a731
Saniya Paswan <saniyapaswan5@gmail.com>
2025-12-06 21:23:58 +0530

    portfolio added

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".", // Project root
-  build: {
-    outDir: "dist", // Build output folder
-    rollupOptions: {
-      input: path.resolve(__dirname, "index.html"),
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
+  base: "/portfolio/", // <<< IMPORTANT
 });
